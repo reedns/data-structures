2518b2541fc4d421ec0a2dfa9e08aee6a13f1104
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-18 16:54:00 -0700

    fix some rubocop offenses

--- lib/radix_sort.rb
+++ lib/radix_sort.rb
@@ -3,14 +3,20 @@ class RadixSort
     rounds = arr.max.to_s.length
 
     rounds.times do |round|
-      buckets = []
-      10.times { buckets << [] }
+      create_buckets_arr
       arr.each do |ele|
-        bucket_number = (ele / 10 ** round) % 10
+        bucket_number = (ele / 10**round) % 10
         buckets[bucket_number] << ele
         arr = buckets.flatten
       end
     end
     arr.flatten
+  end
+
+  private
+
+  def create_buckets_arr
+    buckets = []
+    10.times { buckets << [] }
   end
 end
