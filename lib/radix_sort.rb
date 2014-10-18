64932609670cea5f8da4f290b2dc4de04fdd7af1
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-18 16:26:46 -0700

    finish radix

--- lib/radix_sort.rb
+++ lib/radix_sort.rb
@@ -1,45 +1,16 @@
 class RadixSort
   def sort(arr)
-    zero = []; one = []; two = []; three = []; four = []; five = []; six = []; seven = []; eight = []; nine = []
-    max = arr.max
-    passes = max.to_s.length
-    bucket = 0
-    n = 1
+    rounds = arr.max.to_s.length
 
-    passes.times do
+    rounds.times do |round|
+      buckets = []
+      10.times { buckets << [] }
       arr.each do |ele|
-        case
-        when ele == bucket
-          zero << ele
-        when ele == bucket + 1 * n
-          one.push(ele)
-        when ele == bucket + 2 * n
-          two << ele
-        when ele == bucket + 3 * n
-          three << ele
-        when ele == bucket + 4 * n
-          four << ele
-        when ele == bucket + 5 * n
-          five << ele
-        when ele == bucket + 6 * n
-          six << ele
-        when ele == bucket + 7 * n
-          seven << ele
-        when ele == bucket + 8 * n
-          eight << ele
-        else
-          nine << ele
-        end
+        bucket_number = (ele / 10 ** round) % 10
+        buckets[bucket_number] << ele
+        arr = buckets.flatten
       end
-      n *= 10
     end
-    arr = zero + one + two + three + four + five + six + seven + eight + nine
+    arr.flatten
   end
 end
-
-# it evaluates the ones place of each number in the array
-#   then it puts each number in a corresponding 0-9 bucket
-#     then it puts the numbers back in the array 0s first 9s last
-#       if at least one number has a 10s place it puts the corresponding the numbers
-#       in the corresponding 0-9 bucket; any number that is less than 10 goes in the zero bucket in order
-#         it repeats until the number with the highest number place is sorted
