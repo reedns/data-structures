a32c98d48199793d9ca2c8a6d8a0e5633f618613
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-18 16:26:46 -0700

    finish radix

--- spec/sorts/radix_sort_spec.rb
+++ spec/sorts/radix_sort_spec.rb
@@ -3,15 +3,36 @@ require 'radix_sort.rb'
 
 describe 'RadixSort' do
   describe '#sort' do
-    before { @radix = RadixSort.new }
+    let(:radix) { RadixSort.new }
     it 'returns an array of of single digit numbers in ascending order' do
       arr = [3, 1, 2]
-      @radix.sort(arr).must_equal [1, 2, 3]
+      radix.sort(arr).must_equal [1, 2, 3]
     end
 
-    it 'returns an array of double digit numbers in ascending order' do
+    it 'returns a sorted array of double digit numbers in ascending order' do
       arr = [31, 28, 43]
-      @radix.sort(arr).must_equal [28, 31, 43]
+      radix.sort(arr).must_equal [28, 31, 43]
+    end
+
+    it 'returns a sorted array of n numbers in ascending order' do
+      arr = (1..1_000).to_a.shuffle
+      radix.sort(arr).must_equal (1..1_000).to_a
+    end
+
+    it 'is benchmarked' do
+      puts 'Radix sort:'
+      arr = (1..1_000).to_a
+      puts 'Best Case:'
+      puts Benchmark.measure { radix.sort(arr) }
+
+      arr = (1..1_000).to_a.reverse
+      puts 'Worst Case:'
+      puts Benchmark.measure { radix.sort(arr) }
+
+      arr = (1..1_000).to_a.shuffle
+      puts 'Random:'
+      puts Benchmark.measure { radix.sort(arr) }
+      puts '____________________________________________'
     end
   end
 end
