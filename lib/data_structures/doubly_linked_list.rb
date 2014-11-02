d7159aeb7a38d519ec417cd33078fd45dd2fff70
Reed Nelson-Saunders <reedns@hotmail.com>
2014-11-01 20:20:18 -0700

    deduplicate a linked list

---
+++ lib/data_structures/doubly_linked_list.rb
@@ -0,0 +1,41 @@
+require 'data_structures/singly_linked_list.rb'
+
+Node = Struct.new(:val, :nxt, :prev)
+
+class DoublyLinkedList < LinkedList
+  attr_accessor :head, :tail, :size
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
+  end
+
+  def insert(value)
+    return @head = Node.new(value, nil, nil) if @head.nil?
+    old_head = @head
+    @head = Node.new(value, old_head, nil)
+    old_head.prev = @head
+    old_head = @tail if old_head.nxt.nil?
+    @size += 1
+  end
+
+  def remove(value)
+    node_to_delete = search(value)
+    return nil if node_to_delete.nil?
+    node_to_delete.prev.nxt = node_to_delete.nxt unless node_to_delete == @head
+    node_to_delete.nxt.prev = node_to_delete.prev unless node_to_delete == @tail
+    @size -= 1
+  end
+
+  def remove_duplicates
+    uniq_eles = []
+    current_node = @head
+
+    while current_node
+      uniq_eles << current_node.val unless uniq_eles.include?(current_node.val)
+      current_node = current_node.nxt
+    end
+    uniq_eles
+  end
+end
---
+++ spec/data_structures/doubly_linked_list_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+require 'data_structures/doubly_linked_list.rb'
+
+describe 'DoublyLinkedList' do
+  let(:list) { DoublyLinkedList.new }
+
+  describe '#insert' do
+    it 'inserts a new node the head of the list if the head is nil' do
+      list = DoublyLinkedList.new
+      list.insert('A')
+      list.head.val.must_equal 'A'
+    end
+
+    it 'inserts a new node at the head of the list if there are other nodes' do
+      list.insert('A')
+      list.head.val.must_equal 'A'
+    end
+
+    it 'links the old head to the new head' do
+      list.insert('B')
+      list.insert('A')
+      list.head.nxt.prev.val.must_equal 'A'
+    end
+
+    it 'links the nodes correctly' do
+      list.insert('A')
+      list.insert('B')
+      list.insert('C')
+      list.to_s.must_equal 'C, B, A'
+    end
+  end
+
+  describe '#remove(val)' do
+    it 'removes the node with the same value as the argument from the list' do
+      list.insert('A')
+      list.insert('B')
+      list.insert('C')
+      list.remove('B')
+      list.to_s.must_equal 'C, A'
+    end
+
+    it 'returns nil if node does not exist' do
+      list.insert('A')
+      list.remove('C').must_equal nil
+    end
+  end
+
+  describe '#remove_duplicates' do
+    it 'deletes all the duplicates from the list' do
+      dupe_list = DoublyLinkedList.new()
+      nodes = []
+      200.times { nodes << Node.new(rand(1..100), nil, nil) }
+      #require 'pry'; binding.pry
+      nodes.each { |n|  dupe_list.insert(n) }
+
+      dupe_list.remove_duplicates.to_s
+    end
+  end
+end
